66a14793aad7c4962c7bf15d2f77f04296ce6885
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-07-15 14:46:39 +1000

    Dockerize node, Add route run which creates a container and runs script section of request.body within container

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,68 @@
 import express from "express";
 import Docker from "dockerode";
-
+import tar from "tar-fs";
+import fs from "fs";
+import path from "path";
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Docker API setup
-const docker = new Docker();
-
-// Used to parse JSON bodies, size limited for security
+// Used to parse JSON bodies, size limited
 app.use(express.json({ limit: "300kb" }));
 
-app.get("/", async (_, res) => {
-	console.log("hello");
+// Docker API setup
+// windows: { host: "https://localhost", port: 2375 }
+// linux: { socketPath: "/var/run/docker.sock" }
+const docker = new Docker({ socketPath: "/var/run/docker.sock" });
 
-	console.log(await docker.listContainers());
-
-	// docker.createContainer(
-	// 	{ Image: "ubuntu", Cmd: ["/bin/bash"], name: "ubuntu-test" },
-	// 	function (err, container) {
-	// 		try {
-	// 			container.start(function (err, data) {
-	// 				console.log(data);
-	// 			});
-	// 		} catch (err) {
-	// 			console.log(err);
-	// 		}
-	// 	}
-	// );
-
-	res.status(200).send();
+app.get("/", (req, res) => {
+	res.status(200).send("listening!");
 });
 
-app.post("/run", (req, res) => {
-	const body = req.body;
-	res.status(200).send(body);
+app.post("/run", async (req, res) => {
+	console.log("begin");
+	try {
+		// get script from req and save to file
+		const script = req.body.script;
+		fs.writeFile("./langs/python/app.py", script, function (err) {
+			if (err) throw err;
+		});
+
+		// pack into tar for docker API engine
+		const pack = tar.pack(path.join(__dirname, "../langs/python"));
+
+		// promise stream while docker builds image
+		const stream = await docker.buildImage(pack, {
+			t: "pythonapp",
+		});
+
+		// promise stream resolve
+		await new Promise((resolve, reject) => {
+			docker.modem.followProgress(stream, (err: unknown, res: unknown) =>
+				err ? reject(err) : resolve(res)
+			);
+		});
+
+		// run container
+		await docker.run("pythonapp", [], process.stdout, {
+			name: "pythonapp",
+			AttachStdin: true,
+			AttachStdout: true,
+			AttachStderr: true,
+			Tty: true,
+			OpenStdin: true,
+			StdinOnce: false,
+		});
+
+		// clean up after app concludes
+		const container = docker.getContainer("pythonapp");
+		await container.wait();
+		await container.remove();
+
+		res.status(200).send();
+	} catch (err) {
+		console.log(err);
+		res.status(500).send(err);
+	}
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
