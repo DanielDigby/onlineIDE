d3bfc1d3bf309427e9a0ed0e870f00c605c5399d
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-07-18 11:49:52 +1000

    Add mocha chai

---
+++ backend/test/main_test.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+// Test suite
+describe("Mocha", function () {
+	// Test spec (unit test)
+	it("should run our tests using npm", function () {
+		expect(true).to.be.ok;
+	});
+});
